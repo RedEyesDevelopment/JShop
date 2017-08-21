cd4593769b394b2ed6d3764177ab784a1ec6eb41
Gvo3d <denis.yakimov89@gmail.com>
2017-08-21 23:59:35 +0300

    JShop DATE: 21-08-2017 AUTHOR: Gvozd VERSION: 0.0.2 BRANCH TASK: Create template project
    SITUATION: Nothing
    RECENT CHANGES: Empty template
    NEXT: All
    
    COMMENT: I wanna coffee...

---
+++ src/main/java/jshop/configuration/JDBCConfiguration.java
@@ -0,0 +1,84 @@
+package jshop.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.Locale;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = {"qroktask"})
+public class JDBCConfiguration implements TransactionManagementConfigurer {
+
+    @Value("${dataSource.driverClassName}")
+    private String driver;
+    @Value("${dataSource.url}")
+    private String url;
+    @Value("${dataSource.username}")
+    private String username;
+    @Value("${dataSource.dialect}")
+    private String dialect;
+    @Value("${dataSource.hbm2ddlAuto}")
+    private String hbm2ddlAuto;
+    @Value("${dataSource.package}")
+    private String packageToScan;
+
+    @Bean
+    public DataSource dataSource() {
+        Locale.setDefault(Locale.ENGLISH);
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        EmbeddedDatabase db = builder
+                .setType(EmbeddedDatabaseType.H2) //.H2 or .DERBY
+                .addScript("sql/schemecreation.sql")
+                .addScript("sql/datainsertion.sql")
+                .build();
+        return db;
+    }
+
+    // Start WebServer, access http://localhost:8082
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public org.h2.tools.Server startDBWebManager() throws SQLException {
+        return org.h2.tools.Server.createWebServer();
+    }
+
+    @Bean(name = "transactionManager")
+    public PlatformTransactionManager annotationDrivenTransactionManager() {
+        return new JpaTransactionManager();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSource());
+        entityManagerFactoryBean.setPackagesToScan(packageToScan);
+        entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+        Properties jpaProperties = new Properties();
+        jpaProperties.put(org.hibernate.cfg.Environment.DIALECT, dialect);
+        jpaProperties.put(org.hibernate.cfg.Environment.HBM2DDL_AUTO, hbm2ddlAuto);
+        entityManagerFactoryBean.setJpaProperties(jpaProperties);
+        entityManagerFactoryBean.setDataSource(dataSource());
+        return entityManagerFactoryBean;
+    }
+
+}
