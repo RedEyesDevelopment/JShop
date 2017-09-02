d269347293384011406d7066d98e72dbd602b12e
Gvo3d <denis.yakimov89@gmail.com>
2017-09-02 15:52:22 +0300

    JShop DATE: 02-09-2017 AUTHOR: Gvozd VERSION: 0.0.8 BRANCH TASK: Create template project
    SITUATION: Nothing
    RECENT CHANGES: Configured build.gradle
    NEXT: Create liquibase files for database creation, try to launch data_processor to connect DB in docker container.
    
    COMMENT: I wanna coffee...

--- data_processor/src/main/java/jshop/configuration/JDBCConfiguration.java
+++ data_processor/src/main/java/jshop/configuration/JDBCConfiguration.java
@@ -1,28 +1,30 @@
 package jshop.configuration;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.TransactionManagementConfigurer;
+import projectpackage.repository.reacteav.ReactEAVManager;
+import projectpackage.repository.reacteav.support.ReactConstantConfiguration;
+import projectpackage.repository.reacteav.support.ReactEntityValidator;
+import projectpackage.repository.support.RollbackableTransactionalCustomizer;
 
 import javax.sql.DataSource;
-import java.sql.SQLException;
+import java.beans.PropertyVetoException;
 import java.util.Locale;
-import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
-@EnableJpaRepositories(basePackages = {"jshop"})
 public class JDBCConfiguration implements TransactionManagementConfigurer {
+
+    private static final Logger LOGGER = Logger.getLogger(JDBCConfiguration.class);
 
     @Value("${dataSource.driverClassName}")
     private String driver;
@@ -32,55 +34,94 @@ public class JDBCConfiguration implements TransactionManagementConfigurer {
     private String username;
     @Value("${dataSource.password}")
     private String password;
+    @Value("${dataSource.package}")
+    private String modelPackage;
     @Value("${dataSource.dialect}")
     private String dialect;
-    @Value("${dataSource.hbm2ddlAuto}")
-    private String hbm2ddlAuto;
-    @Value("${dataSource.package}")
-    private String packageToScan;
+
+
+    public JDBCConfiguration() {
+        Locale.setDefault(Locale.ENGLISH);
+    }
 
     @Bean
     public DataSource dataSource() {
-        Locale.setDefault(Locale.ENGLISH);
+        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
         try {
             Class.forName(driver);
         } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            LOGGER.error("Driver not found!", e);
         }
+        try {
+            comboPooledDataSource.setDriverClass(driver);
+        } catch (PropertyVetoException e) {
+            LOGGER.error("Driver not supported!", e);
+        }
+        //ссылка
+        comboPooledDataSource.setJdbcUrl(url);
+        //логин
+        comboPooledDataSource.setUser(username);
+        //пароль
+        comboPooledDataSource.setPassword(password);
+        //минимальный размер пула
+        comboPooledDataSource.setMinPoolSize(5);
+        //максимальный размер пула
+        comboPooledDataSource.setMaxPoolSize(40);
+        //начальный размер пула
+        comboPooledDataSource.setInitialPoolSize(10);
+        //сколько пулов разрешено взять поверх максимального числа
+        comboPooledDataSource.setAcquireIncrement(10);
+        //максимальное время получения содеинения под запрос
+        comboPooledDataSource.setMaxIdleTime(300);
+        //максимальное время жизни запроса
+        comboPooledDataSource.setMaxConnectionAge(1200);
+        //время простоя соединения, после которого оно уничтожается, пул сжимается до минимума
+        comboPooledDataSource.setMaxIdleTimeExcessConnections(120);
+        //время между повторами запроса на соединение
+        comboPooledDataSource.setAcquireRetryDelay(1500);
+        //размер кэша под preparestatements
+        comboPooledDataSource.setMaxStatements(500);
+        //размер кэша для одного соединения под preparestatements
+        comboPooledDataSource.setMaxStatementsPerConnection(14);
+        //время через которое проверяется соединение на состояние
+        comboPooledDataSource.setIdleConnectionTestPeriod(300);
+        //имя специальной таблицы для тестирования соединения с БД
+        comboPooledDataSource.setAutomaticTestTable("c3p0DatabaseTestTable");
+        comboPooledDataSource.setForceIgnoreUnresolvedTransactions(true);
+        comboPooledDataSource.setAutoCommitOnClose(false);
+        comboPooledDataSource.setConnectionCustomizerClassName(RollbackableTransactionalCustomizer.class.getName());
+        comboPooledDataSource.setNumHelperThreads(10);
 
-        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-        EmbeddedDatabase db = builder
-                .setType(EmbeddedDatabaseType.H2) //.H2 or .DERBY
-                .addScript("sql/schemecreation.sql")
-                .addScript("sql/datainsertion.sql")
-                .build();
-        return db;
+        return comboPooledDataSource;
     }
 
-    // Start WebServer, access http://localhost:8082
-    @Bean(initMethod = "start", destroyMethod = "stop")
-    public org.h2.tools.Server startDBWebManager() throws SQLException {
-        return org.h2.tools.Server.createWebServer();
-    }
-
-    @Bean(name = "transactionManager")
+    @Override
+    @Bean
     public PlatformTransactionManager annotationDrivenTransactionManager() {
-        return new JpaTransactionManager();
+        DataSourceTransactionManager dataSourceTransactionManager = new DataSourceTransactionManager();
+        dataSourceTransactionManager.setDataSource(dataSource());
+        return dataSourceTransactionManager;
     }
 
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
-        entityManagerFactoryBean.setDataSource(dataSource());
-        entityManagerFactoryBean.setPackagesToScan(packageToScan);
-        entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+    NamedParameterJdbcTemplate namedParameterJdbcTemplate(){
+        return new NamedParameterJdbcTemplate(dataSource());
+    }
 
-        Properties jpaProperties = new Properties();
-        jpaProperties.put(org.hibernate.cfg.Environment.DIALECT, dialect);
-        jpaProperties.put(org.hibernate.cfg.Environment.HBM2DDL_AUTO, hbm2ddlAuto);
-        entityManagerFactoryBean.setJpaProperties(jpaProperties);
-        entityManagerFactoryBean.setDataSource(dataSource());
-        return entityManagerFactoryBean;
+    @Bean
+    JdbcTemplate jdbcTemplate () { return new JdbcTemplate(dataSource()); }
+
+    @Bean
+    ReactConstantConfiguration reactConstantConfiguration() { return new ReactConstantConfiguration(); }
+
+    @Bean
+    ReactEAVManager reactEAVManager(){
+        return new ReactEAVManager(reactConstantConfiguration(),modelPackage);
+    }
+
+    @Bean
+    ReactEntityValidator reactEntityValidator(){
+        return new ReactEntityValidator();
     }
 
 }
