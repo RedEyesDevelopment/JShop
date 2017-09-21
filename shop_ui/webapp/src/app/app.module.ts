e36ca25808c76d78ab07038cd83d7e0d18642558
Tamashchuk Vladislav <tamashchuk.v@gmail.com>
2017-09-22 00:37:57 +0300

    new ang cli

--- shop_ui/webapp/src/app/app.module.ts
+++ shop_ui/webapp/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { AppSlider } from './app.slider';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppSlider
   ],
   imports: [
     BrowserModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
